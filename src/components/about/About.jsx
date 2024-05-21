c83cf592d4f0d024cace81075b35ba7652ef7244
Sumanth H <sumanthh30062002@gmail.com>
2024-05-21 20:41:13 +0530

    info.jsx fixed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,6 @@ import "./about.css"
 import sumanthbg from "../../assets/sumanth-bgrm.png"
 import sumanthwater from "../../assets/12706.jpg"
 import CV from "../../assets/sumanth_resume.pdf"
-import Information from "../about/Information"
 
 export const About = () => {
   return (
@@ -14,7 +13,6 @@ export const About = () => {
         <div className="about__container container grid">
             <img src={sumanthbg} alt="" className="about__img" />
             <div className="about__data">
-               <Information/> 
               <p className="about__description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, dolorum placeat, pariatur ea autem tempore dignissimos officiis dolorem consequuntur atque, sunt exercitationem velit. 
               Architecto ullam quas, impedit aut possimus iste?</p>
             </div>
